d0591c63720bfd6a8ab547cabb6715bab9970138
Ashwin Fernandes <ashphoenix16@gmail.com>
2017-02-01 12:52:50 +0530

    RSA Cryptographic Algorithm

---
+++ RSA.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+int a,M,n,e,d,C,FLAG;
+int check()
+  {
+   int i;
+   for(i=3;e%i==0 && a%i==0;i+2)
+     {
+      FLAG = 1;
+      return;
+     }
+   FLAG = 0;
+  }
+
+void encrypt()
+  {
+      int i;
+      C = 1;
+      for(i=0;i< e;i++)
+      C=C*M%n;
+      C = C%n;
+      printf("\nEncrypted keyword : %d",C);
+  }
+
+void decrypt()
+  {
+      int i;
+      M = 1;
+      for(i=0;i< d;i++)
+      M=M*C%n;
+      M = M%n;
+      printf("\nDecrypted keyword : %d\n",M);
+  }
+
+void main()
+  {
+   
+
+   int p,q,s;
+   printf("Enter Two Relatively Prime Numbers\t: ");
+   scanf("%d%d",&p,&q);
+   n = p*q;
+   printf("\n\tValue of n\t= %d",n);
+   a=(p-1)*(q-1);
+   printf("\n\tF(n)\t= %d",a);
+   do
+     {
+      printf("\n\nEnter e\t: ");
+      scanf("%d",&e);
+      check();
+     }
+   while(FLAG==1);
+   d = 1;
+   do
+     {
+      s = (d*e)%a;
+      d++;
+     }
+   while(s!=1);
+   d = d-1;
+   printf("\n\tPublic Key\t: {%d,%d}",e,n);
+   printf("\n\tPrivate Key\t: {%d,%d}",d,n);
+   printf("\n\nEnter The Plain Text\t: ");
+   scanf("%d",&M);
+   encrypt();
+   printf("\n\nEnter the Cipher Text\t:");
+   scanf("%d",&C);
+   decrypt();
+  }
